3e6bf6dd56d3eb274e752d75be258b2ebf5eaa9a
Ret2Hell <tayeb1920@gmail.com>
2026-04-05 21:22:20 +0100

    added MVs

---
+++ migrations/000002_create_analytics_rollups.down.sql
@@ -0,0 +1,15 @@
+DROP VIEW IF EXISTS analytics.mv_page_engagement_daily;
+DROP VIEW IF EXISTS analytics.mv_campaigns_daily;
+DROP VIEW IF EXISTS analytics.mv_device_breakdown_daily;
+DROP VIEW IF EXISTS analytics.mv_country_breakdown_daily;
+DROP VIEW IF EXISTS analytics.mv_referrers_daily;
+DROP VIEW IF EXISTS analytics.mv_page_views_hourly;
+DROP VIEW IF EXISTS analytics.mv_site_metrics_hourly;
+
+DROP TABLE IF EXISTS analytics.page_engagement_daily;
+DROP TABLE IF EXISTS analytics.campaigns_daily;
+DROP TABLE IF EXISTS analytics.device_breakdown_daily;
+DROP TABLE IF EXISTS analytics.country_breakdown_daily;
+DROP TABLE IF EXISTS analytics.referrers_daily;
+DROP TABLE IF EXISTS analytics.page_views_hourly;
+DROP TABLE IF EXISTS analytics.site_metrics_hourly;
